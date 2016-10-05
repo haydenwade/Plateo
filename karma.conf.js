ff3b4a37fe47c1c563831742b9b0ced5ba97d8c8
haydenty <htwade51@gmail.com>
2016-10-04 21:16:05 -0500

    changed karma.conf - switch from chrome to firefox

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'app/**/*.js , tests/**/*.js'
+      'server.js , tests/**/*.js'
     ],
 
 
@@ -56,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['FireFox'],
 
 
     // Continuous Integration mode
