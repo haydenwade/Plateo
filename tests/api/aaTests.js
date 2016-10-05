1c2162f3f94f63b121399e7baf665f5e4e0464ef
haydenty <htwade51@gmail.com>
2016-10-04 20:32:32 -0500

    added karma, jasmine, travis CI

---
+++ tests/api/aaTests.js
@@ -0,0 +1,13 @@
+var request = require("request"); //Nodes package for sending http requests to server
+var baseUrl = "http:localhost:3000/";
+
+describe("Automated Acceptence Tests for Plateo Api", function (){
+  describe("GET /", function (){
+      it("should return status code 200 O.K.", function(){
+          request.get(baseUrl, function(error, response, body){
+              expect(response.statusCode).toEqual(200);
+              done();//node.js is an asynchronous environment, the it function will finish before our expect function does
+          });
+      });
+  });
+});
